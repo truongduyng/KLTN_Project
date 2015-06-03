e37030497fec773ec86cc4a3512c1609b6a1bd48
truongduyng <1112046@student.hcmus.edu.vn>
2015-06-03 08:17:51 +0700

    update

--- app/assets/javascripts/adminJS/bussinessAdmin/tickets_management/ticketManageCtrl.js
+++ app/assets/javascripts/adminJS/bussinessAdmin/tickets_management/ticketManageCtrl.js
@@ -250,7 +250,7 @@ $scope.showtimeintd = function(hour,element,show){
       $('.tablebooking').css({width: $('.calendar_content').width()});
 
     var scrollheight = $scope.td_height*4*(24-0);
-    $('hr.timeline').css({width: 170*$scope.branch.assets.length + 50});
+    $('hr.timeline').css({width: $('.tablebooking').width()});
 
     var top_timeline = 23 + Math.floor((parseInt($scope.dt.getHours())*60+parseInt($scope.dt.getMinutes()))*scrollheight/(60*24)); // 23 is height of th
     $('hr.timeline').animate({top: top_timeline},'fast');
