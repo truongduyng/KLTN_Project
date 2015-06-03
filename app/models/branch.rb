dd97d53e5a77a2c2e7aa748598b5f1e9ae771074
Huu Trung <trungnguyenhuu262@gmail.com>
2015-06-03 10:09:52 +0700

    fix conflict in model Role, model Bussiness, model Branch

--- app/models/branch.rb
+++ app/models/branch.rb
@@ -1,26 +1,3 @@
-<<<<<<< HEAD
-class Branch 
-	include Mongoid::Document
-	include Mongoid::Timestamps
-	
-	field :name, type: String
-	field :address, type: String
-	field :phone, type: String
-	field :latitude, type: Float
-	field :longitude, type: Float
-	field :description, type: String
-	
-	belongs_to :bussiness
-	has_many :assests
-
-	#validates
-	validates :name, presence: true,  length: {maximum: 1000}
-	validates :address, presence: true, length: {maximum: 1000}
-	validates :latitude, presence: true
-	validates :longitude, presence: true
-	
-end
-=======
 class Branch
   include Mongoid::Document
   include Geocoder::Model::Mongoid
@@ -86,4 +63,3 @@ class Branch
     end
   end
 end
->>>>>>> merge_bussiness_admin
--- app/models/bussiness.rb
+++ app/models/bussiness.rb
@@ -1,33 +1,4 @@
 class Bussiness
-<<<<<<< HEAD
-	include Mongoid::Document
-	field :name, type: String
-	#address bo thay the boi address cua branch
-	field :address, type: String
-	field :phone, type: String
-	field :category, type: String #Linh vuc
-	field :short_desc, type: String
-	field :description, type: String
-	#Moi bussiness co 1 avatar
-	field :avatar, type: String
-	mount_uploader :avatar, AvatarUploader
-	#Thuoc tinh cho thay 1 bussiness do co bi cam hay ko
-	#mac dinh la ko bi cam
-	field :is_banned, type: Boolean, default: ->{false}
-	belongs_to :user
-	has_many :assest_categories
-	has_many :branches
-
-	# before_create :init_avatar
-	
-	# private
-	# 	#chua test
-	# 	#Khoi tao avatar mac dinh
-	# 	def init_avatar
-	# 		self.avatar = current_user.avatar
-	# 	end
-end
-=======
   include Mongoid::Document
   field :name, type: String
   field :address, type: String
@@ -36,7 +7,15 @@ end
   field :short_desc, type: String
   field :description, type: String
 
+  ##BN
+  field :avatar, type: String
+  mount_uploader :avatar, AvatarUploader
+  #Thuoc tinh cho thay 1 bussiness do co bi cam hay ko
+	#mac dinh la ko bi cam
+  field :is_banned, type: Boolean, default: ->{false}
+  ##BN
+
   belongs_to :user
   has_many :branches
 end
->>>>>>> merge_bussiness_admin
+
