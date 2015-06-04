4eac18f7ac13a0e17fbdc62aaedef6764568624b
truongduyng <1112046@student.hcmus.edu.vn>
2015-06-04 08:45:18 +0700

    update

--- app/assets/javascripts/appJS/club/clubCtrl.js
+++ app/assets/javascripts/appJS/club/clubCtrl.js
@@ -1,16 +1,4 @@
 app.controller('clubCtrl',['$scope', '$modal', function($scope, $modal){
 
-  $scope.opennewclub = function(){
-    var newclubmodal = $modal.open({
-      templateUrl: 'newclub.html',
-      controller: 'newclubmodalCtrl',
-      animation: false,
-      windowClass: 'newclubmodal'
-    });
-  }
 
 }]);
-
-app.controller('newclubmodalCtrl', function($modalInstance){
-
-});
