a03d01513b5b7f1ab38b24e77cc82be499cc2dcf
Huu Trung <trungnguyenhuu262@gmail.com>
2015-06-10 15:59:25 +0700

    fix lỗi authenticate token khi đăng xuất và login lại tức thời

--- app/assets/javascripts/appJS/venue/venueDetail/venueDetailCtrl.js
+++ app/assets/javascripts/appJS/venue/venueDetail/venueDetailCtrl.js
@@ -1,4 +1,21 @@
 app.controller('venueDetailCtrl', ['$scope', 'venueDetailService', function ($scope, venueDetailService) {
 		$scope.venue = venueDetailService.venue;
+		
+		$scope.showImage = function(photo) {
+			var modalInstance = $modal.open({
+				templateUrl: 'showImageModal.html',
+				controller: 'showImageModalCtrl',
+				size: 'lg',
+				resolve: {
+					photo: function() {
+						return photo;
+					},
+					listPhotos: function() {
+						return $scope.venue.photos;
+					}
+				}
+			});
+
+		};
 
 }]);
