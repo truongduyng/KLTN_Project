a62a7f1d3c7412f1a66f5f00c22b92bfe5dc19fc
truongduyng <1112046@student.hcmus.edu.vn>
2015-06-17 11:37:51 +0700

    update UI post

--- app/assets/javascripts/appJS/home/posts/listPostCtrl.js
+++ app/assets/javascripts/appJS/home/posts/listPostCtrl.js
@@ -1,11 +1,16 @@
 app.controller('listPostCtrl', ['$scope', 'listPostService', 'Auth', function($scope, listPostService, Auth) {
 
 	$scope.posts = listPostService.posts;
-	console.log($scope.posts);
+
 	$scope.currentPage = 1;
 	$scope.isBusy = false;
 	$scope.isEnd = false;
 
+	$.each($('div.post-item'), function(index, item){
+    console.log(item);
+  });
+
+  console.log($('div.post-item').attr('class'));
 
 	$scope.loadMore = function(){
 		if($scope.isEnd){
@@ -20,7 +25,6 @@ app.controller('listPostCtrl', ['$scope', 'listPostService', 'Auth', function($s
 			if(data == null || data.length == 0){
 				$scope.isEnd = true;
 			}
-			console.log($scope.posts);
 
 			$scope.isBusy = false;
 		}).error(function(){
