0fccb3438081b64c3995ab80565553ef98a5664a
truongduyng <1112046@student.hcmus.edu.vn>
2015-06-29 21:43:55 +0700

    fix real time

--- app/controllers/authorization_controller.rb
+++ app/controllers/authorization_controller.rb
@@ -1,11 +1,13 @@
 class AuthorizationController < WebsocketRails::BaseController
 
   def authorize_channels
-    if user_signed_in? && current_user.can_subcribe_channel?(message[:channel])
-      WebsocketRails[message[:channel]].make_private
-      accept_channel current_user
+    if current_user
+      if current_user.can_subcribe_channel?(message[:channel])
+        WebsocketRails[message[:channel]].make_private
+        accept_channel current_user
+      end
     else
-     	deny_channel({:message => 'authorization failed!'})
+      deny_channel({:message => 'authorization failed!'})
     end
   end
 
