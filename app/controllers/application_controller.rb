89d059f45cac1cd1d917c4f37e649c027e07a4a7
truongduyng <1112046@student.hcmus.edu.vn>
2015-06-30 11:27:27 +0700

    update

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,7 @@
 class ApplicationController < ActionController::Base
 
   protect_from_forgery with: :exception
-  
+
   after_filter :set_csrf_cookie_for_ng
   def set_csrf_cookie_for_ng
     cookies['XSRF-TOKEN'] = form_authenticity_token if protect_against_forgery?
@@ -34,7 +34,7 @@ class ApplicationController < ActionController::Base
       devise_parameter_sanitizer.for(:sign_up) << [:username, :fullname, :phone]
       devise_parameter_sanitizer.for(:account_update) << [:username, :fullname, :phone, :role_name]
     end
-    # In Rails 4.2 and above
+
     def verified_request?
       super || valid_authenticity_token?(session, request.headers['X-XSRF-TOKEN'])
     end
