c1f4e5839a119e5f0df5d3be884b9b930ca9ddcb
truongduyng <1112046@student.hcmus.edu.vn>
2015-06-30 10:52:40 +0700

    update

--- app/controllers/authorization_controller.rb
+++ app/controllers/authorization_controller.rb
@@ -1,7 +1,7 @@
 class AuthorizationController < WebsocketRails::BaseController
 
   def authorize_channels
-    puts "#{user_signed_in?}-----------------------current_user: #{current_user}------------------"
+    puts "#{user_signed_in?}-----------------------current_user: #{current_user}---------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------"
     if current_user.can_subcribe_channel?(message[:channel])
         WebsocketRails[message[:channel]].make_private
         accept_channel current_user
