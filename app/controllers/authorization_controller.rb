913e11ad45e465659c56d2b39ff83ab5324259db
truongduyng <1112046@student.hcmus.edu.vn>
2015-06-30 11:05:39 +0700

    update

--- app/controllers/authorization_controller.rb
+++ app/controllers/authorization_controller.rb
@@ -1,13 +1,14 @@
 class AuthorizationController < WebsocketRails::BaseController
 
   def authorize_channels
-    puts "#{user_signed_in?}-----------------------current_user: #{current_user}---------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------"
-    if user_signed_in? || current_user.can_subcribe_channel?(message[:channel])
-        WebsocketRails[message[:channel]].make_private
-        accept_channel current_user
-    else
-      deny_channel({:message => 'authorization failed!'})
-    end
+    # puts "#{user_signed_in?}-----------------------current_user: #{current_user}---------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------"
+    # if user_signed_in? || current_user.can_subcribe_channel?(message[:channel])
+    #     WebsocketRails[message[:channel]].make_private
+    #     accept_channel current_user
+    # else
+    #   deny_channel({:message => 'authorization failed!'})
+    # end
+    deny_channel({:message => "#{user_signed_in?}-----#{current_user}-----authorization failed!"})
   end
 
 end
