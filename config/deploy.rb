8df6e7e4dca4b07fdd167d241b6b0d15b3d1cc9f
truongduyng <1112046@student.hcmus.edu.vn>
2015-06-30 20:32:21 +0700

    update

--- config/deploy.rb
+++ config/deploy.rb
@@ -47,11 +47,11 @@ namespace :deploy do
   end
 
   desc 'Restart application'
-  task :restart do
-    on roles(:app), in: :sequence, wait: 5 do
-      execute :touch, release_path.join('tmp/restart.txt')
-    end
-  end
+  # task :restart do
+  #   on roles(:app), in: :sequence, wait: 5 do
+  #     execute :touch, release_path.join('tmp/restart.txt')
+  #   end
+  # end
 
   after :publishing, 'deploy:restart'
   after :finishing, 'deploy:cleanup'
