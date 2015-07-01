21c83ae13bf41c3415853fc93ff704ec0ad9496b
truongduyng <1112046@student.hcmus.edu.vn>
2015-07-01 08:26:19 +0700

    update

--- config/initializers/websocket_rails.rb
+++ config/initializers/websocket_rails.rb
@@ -18,7 +18,7 @@ WebsocketRails.setup do |config|
   config.standalone = false
   # config.redis_options = {:host => 'localhost', :port => '6379'}
   config.redis_options = {:host => '128.199.176.52', :port => '6379'}
-  # config.standalone_port = 3001
+  config.standalone_port = 3001
   # Uncomment and edit to point to a different redis instance.
   # Will not be used unless standalone or synchronization mode
   # is enabled.
