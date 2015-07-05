face7e69225fe767de61fafd7d54bb731f675097
truongduyng <1112046@student.hcmus.edu.vn>
2015-07-05 12:56:42 +0700

    fix booking ticket

--- app/assets/javascripts/appJS/all_likes/alllikesCtrl.js
+++ app/assets/javascripts/appJS/all_likes/alllikesCtrl.js
@@ -1,6 +1,12 @@
 app.controller('alllikesCtrl', ['$scope', 'service_get_like', 'object_get_like', function($scope, service_get_like, object_get_like) {
 
   $scope.isLoading = true;
+
+  $scope.go_to_user_page = function(){
+    $scope.$close();
+    $state.go("trangCaNhan",{username:like.user.username});
+  }
+
   service_get_like.getAllLikes(object_get_like).success(function(data){
     $scope.allLikes = data;
     $scope.isLoading = false;
