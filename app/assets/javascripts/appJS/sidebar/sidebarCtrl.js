eb79bcd1e799cfe396c0f75d64adcaacddd0fc7e
Huu Trung <trungnguyenhuu262@gmail.com>
2015-07-18 18:34:54 +0700

    hoàn tất giao diện thêm sở thích, chuẩn bi thêm autocomplete

--- app/assets/javascripts/appJS/sidebar/sidebarCtrl.js
+++ app/assets/javascripts/appJS/sidebar/sidebarCtrl.js
@@ -1,81 +1,103 @@
-app.controller('sidebarCtrl',['$scope', '$modal', 'clubsFtry', '$state', 'Auth', function($scope, $modal, clubsFtry, $state, Auth){
+app.controller('sidebarCtrl', ['$scope', '$modal', 'clubsFtry', '$state', 'Auth',
+  function($scope, $modal, clubsFtry, $state, Auth) {
 
-  Auth.currentUser().then(function(user) {
-    $scope.user = user;
-    $scope.loadClubs();
-  }, function(error) {});
+    Auth.currentUser().then(function(user) {
+      $scope.user = user;
+      $scope.loadClubs();
+    }, function(error) {});
 
-  $scope.$on('devise:new-session', function(e, user) {
-    $scope.user = user;
-    $scope.loadClubs();
-  });
-
-  $scope.$on('devise:new-registration', function(e, user) {
-    $scope.user = user;
-    $scope.loadClubs();
-  });
-
-  $scope.$on('devise:login', function(e, user) {
-    $scope.user = user;
-    $scope.loadClubs();
-  });
-
-  $scope.$on('devise:logout', function(e, user) {
-    $scope.user = {};
-    $scope.clubs ={};
-  });
-
-  $scope.$on('onChangeUserProfile', function(event, user){
-    angular.copy(user, $scope.user);
-  });
-
-  $scope.loadClubs = function(){
-    clubsFtry.index().success(function(data){
-      $scope.clubs = data;
-      console.log(data);
-    })
-  }
-
-  $scope.opennewclub = function(){
-    var newclubmodal = $modal.open({
-      templateUrl: 'appJS/sidebar/_new_club.html',
-      controller: 'newclubmodalCtrl'
+    $scope.$on('devise:new-session', function(e, user) {
+      $scope.user = user;
+      $scope.loadClubs();
     });
 
-    newclubmodal.result.then(function (club) {
-      console.log(club);
-      clubsFtry.create(club).success(function(result){
-        $state.go('club',{club_id: result.id.$oid, club_post_id: null});
+    $scope.$on('devise:new-registration', function(e, user) {
+      $scope.user = user;
+      $scope.loadClubs();
+    });
+
+    $scope.$on('devise:login', function(e, user) {
+      $scope.user = user;
+      $scope.loadClubs();
+    });
+
+    $scope.$on('devise:logout', function(e, user) {
+      $scope.user = {};
+      $scope.clubs = {};
+    });
+
+    $scope.$on('onChangeUserProfile', function(event, user) {
+      angular.copy(user, $scope.user);
+    });
+
+    $scope.loadClubs = function() {
+      clubsFtry.index().success(function(data) {
+        $scope.clubs = data;
+        console.log(data);
+      })
+    }
+
+    $scope.opennewclub = function() {
+      var newclubmodal = $modal.open({
+        templateUrl: 'appJS/sidebar/_new_club.html',
+        controller: 'newclubmodalCtrl'
       });
-    }, function () {
-    });
-  }
 
-  $scope.gotoclub = function(club_id){
-    $state.go('club',{club_id: club_id, club_post_id: null});
-  }
+      newclubmodal.result.then(function(club) {
+        console.log(club);
+        clubsFtry.create(club).success(function(result) {
+          $state.go('club', {
+            club_id: result.id.$oid,
+            club_post_id: null
+          });
+        });
+      }, function() {});
+    }
 
-  $scope.isbussinessadmin = function() {
-      if($scope.user){
-        return _.some($scope.user.roles, function(role){
+    $scope.gotoclub = function(club_id) {
+      $state.go('club', {
+        club_id: club_id,
+        club_post_id: null
+      });
+    }
+
+    $scope.isbussinessadmin = function() {
+      if ($scope.user) {
+        return _.some($scope.user.roles, function(role) {
           return role == 'bussiness admin';
         });
-      }else{
+      } else {
         return false;
       }
     };
 
     $scope.isSystemAdmin = function() {
-      if($scope.user){
-        return _.some($scope.user.roles, function(role){
+      if ($scope.user) {
+        return _.some($scope.user.roles, function(role) {
           return role == 'system admin';
         });
-      }else{
+      } else {
         return false;
       }
     };
 
 
-    
+    //Cho so thich
+    $scope.openInterestModal = function() {
+      var modalInstance = $modal.open({
+        animation: true,
+        templateUrl: 'modalIntereset.html',
+        controller: 'InterestModalCtrl',
+        size: "sm",
+        resolve: {
 
-}]);
+        }
+      });
+    };
+
+  }
+]);
+
+app.controller('InterestModalCtrl', function($scope, $modalInstance) {
+
+});
